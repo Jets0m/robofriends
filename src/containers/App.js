2c977f7705303d86f9439bf926645dbd7629b912
Jets0m <rpbutler@gmail.com>
2020-05-01 18:11:51 +0100

    Upload 3

---
+++ src/containers/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(responce => responce.json())
+      .then(users => {this.setState({robots: users})});
+  }
+
+  onSearch = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return (
+      <div className="tc">
+      <div className="header">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearch}/>
+      </div>
+      <CardList robots={filteredRobots}/>
+      </div>
+    );
+  }
+};
+
+export default App;
