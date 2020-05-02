aa3c4e2212a0a53f50697c0d73e58744d58e836c
Jets0m <rpbutler@gmail.com>
2020-05-02 19:51:18 +0100

    Updates

---
+++ precache-manifest.a3b80b40054530e24b51af549a2feb31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc0b986b4164115244416f7fa1a723a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c3072bf9e6c33a00390f",
+    "url": "/robofriends/static/css/main.c7944d8b.chunk.css"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3072bf9e6c33a00390f",
+    "url": "/robofriends/static/js/main.53965c44.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
